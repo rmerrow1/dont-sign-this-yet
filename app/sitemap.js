bd14946526175bb989b0826d25f1065d769b66b8
rmerrow1 <rmerrow111@gmail.com>
2026-09-24 15:07:29 -0500

    Link dealer checklist and include it in sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -3,6 +3,7 @@ export default function sitemap() {
     {url:"https://www.dontsignthisyet.com/", changeFrequency:"monthly", priority:1},
     {url:"https://www.dontsignthisyet.com/out-the-door-price", changeFrequency:"monthly", priority:.8},
     {url:"https://www.dontsignthisyet.com/dealer-add-ons", changeFrequency:"monthly", priority:.8},
-    {url:"https://www.dontsignthisyet.com/negative-equity", changeFrequency:"monthly", priority:.8}
+    {url:"https://www.dontsignthisyet.com/negative-equity", changeFrequency:"monthly", priority:.8},
+    {url:"https://www.dontsignthisyet.com/dealer-checklist", changeFrequency:"monthly", priority:.8}
   ];
 }
