781abee6ded4ece8ab711f5e6a4d9bdea9d7ae9a
rmerrow1 <rmerrow111@gmail.com>
2026-09-24 11:01:55 -0500

    Align canonical and sitemap URLs with www production domain

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,7 +1,7 @@
 export default function sitemap() {
   return [
-    {url:"https://dontsignthisyet.com/", changeFrequency:"monthly", priority:1},
-    {url:"https://dontsignthisyet.com/out-the-door-price", changeFrequency:"monthly", priority:.8},
-    {url:"https://dontsignthisyet.com/dealer-add-ons", changeFrequency:"monthly", priority:.8}
+    {url:"https://www.dontsignthisyet.com/", changeFrequency:"monthly", priority:1},
+    {url:"https://www.dontsignthisyet.com/out-the-door-price", changeFrequency:"monthly", priority:.8},
+    {url:"https://www.dontsignthisyet.com/dealer-add-ons", changeFrequency:"monthly", priority:.8}
   ];
 }
