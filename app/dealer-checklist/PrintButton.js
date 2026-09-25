b8f341a160d3d0d0ed91ac5a08a7abb8c4bbd35f
rmerrow1 <rmerrow111@gmail.com>
2026-09-25 13:35:29 -0500

    Track checklist print clicks

--- app/dealer-checklist/PrintButton.js
+++ app/dealer-checklist/PrintButton.js
@@ -1,5 +1,7 @@
 "use client";
 
+import { track } from "@vercel/analytics";
+
 export default function PrintButton() {
-  return <button className="checklistPrint" type="button" onClick={() => window.print()}>Print checklist</button>;
+  return <button className="checklistPrint" type="button" onClick={() => {track("Checklist Print Clicked"); window.print();}}>Print checklist</button>;
 }
